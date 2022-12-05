32e409e24e05bcada361c32cb4960ca64f638188
modengann <99417989+modengann@users.noreply.github.com>
2022-12-05 12:53:27 -0500

    Update plant_classification.py

--- src/plant_classification.py
+++ src/plant_classification.py
@@ -8,14 +8,7 @@ from sklearn import metrics
 from sklearn.metrics import accuracy_score
 
 def plant_classification():
-    data = load_iris()
-    X, y = data.data, data.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
-    model = naive_bayes.GaussianNB()
-    model.fit(X_train, y_train)
-    y_fitted = model.predict(X_test)
-    result = accuracy_score(y_test, y_fitted)
-    return result
+    pass
 
 def main():
     print(f"Accuracy is {plant_classification()}")
